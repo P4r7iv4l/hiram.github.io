06a82ab410162bc982b8769fe9d0b4f5957afb4d
P4r7iv4l <edgar8j@hotmail.com>
2026-05-19 07:31:30 +0000

    Deploying to gh-pages from @ P4r7iv4l/hiram.github.io@ca88dbedea19a44490eaf2defefb7f97deebe987 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "alshedivat/al-folio",
+    "data-repo": "P4r7iv4l/hiram.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
